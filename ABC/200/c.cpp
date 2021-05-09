d4660d40b4f287d2b5bc28348ce41e7cb1d030ad
keimoriyama <keischwiiz@gmail.com>
2021-05-09 11:46:44 +0900

    ac

--- ABC/200/c.cpp
+++ ABC/200/c.cpp
@@ -10,21 +10,16 @@ int main()
 {
     int N;
     cin >> N;
-    int a[N];
-    int ans = 0;
-    for(int i = 0; i < N; i++)
-        cin >> a[i];
-    //sort(a, a+N);
+    int a[N]; 
+    long long b[200] = {0};
+    long long ans = 0;
     for(int i = 0; i < N; i++){
-        for(int j = i+1; j < N; j++){
-            /*
-            if(a[i]%100 != a[j]%100)continue;
-            if(((a[i]/100)-(a[j]/100))%2 != 0)continue;
-            */
-            if((a[i]-a[j])%200 != 0)continue;
-            //cout << "i: " << i << " j: " << j << endl;
-            ans++;
-        }
+        cin >> a[i];
+        b[a[i]%200]++;
+    }
+    //sort(a, a+N);
+    for(int i = 0; i < 200; i++){
+            ans += (b[i]*(b[i]-1))/2;
     }
     cout << ans << endl;
     return 0;
