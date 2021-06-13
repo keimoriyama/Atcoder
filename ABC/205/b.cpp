f9a4407b6821868483a710422e052468a5143ea5
keimoriyama <keischwiiz@gmail.com>
2021-06-13 22:52:17 +0900

    今日はここまで

---
+++ ABC/205/b.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int N;
+  bool ans = true;
+  cin >> N;
+  vector<int> a(N);
+  for (int i = 0; i < N; i++)
+    cin >> a[i];
+  sort(a.begin(), a.end());
+  for (int i = 0; i < N; i++) {
+    if (a[i] != i + 1)
+      ans = false;
+  }
+  if (ans)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
