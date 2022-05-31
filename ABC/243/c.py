2c38c5605c12eb1e40ba2d03907fb5520bee1eec
keimoriyama <86696267+keimoriyama0097@users.noreply.github.com>
2022-05-31 16:52:53 +0900

    solved 243

--- ABC/243/c.py
+++ ABC/243/c.py
@@ -1,9 +1,31 @@
 N = int(input())
-X, Y  =  [], []
+X, Y = [], []
 for _ in range(N):
-    x,y = map(int, input().split())
+    x, y = map(int, input().split())
     X.append(x)
     Y.append(y)
 S = input()
 
+right_min, left_max = dict(), dict()
+for i in range(N):
+    if S[i] == "R":
+        if Y[i] in left_max and X[i] < left_max[Y[i]]:
+            print("Yes")
+            exit(0)
+    else:
+        if Y[i] in right_min and right_min[Y[i]] < X[i]:
+            print("Yes")
+            exit(0)
 
+    if S[i] == "R":
+        if Y[i] in right_min:
+            right_min[Y[i]] = min(X[i], right_min[Y[i]])
+        else:
+            right_min[Y[i]] = X[i]
+    else:
+        if Y[i] in left_max:
+            left_max[Y[i]] = max(X[i], left_max[Y[i]])
+        else:
+            left_max[Y[i]] = X[i]
+
+print("No")
