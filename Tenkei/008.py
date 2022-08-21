40cd78612dde1455028bb73c40643dd5df06a140
keimoriyama <keischwiiz@gmail.com>
2022-08-21 21:46:48 +0900

    solved 008

--- Tenkei/008.py
+++ Tenkei/008.py
@@ -1,9 +1,9 @@
 N = int(input())
 S = input()
-D = [[0] * (len(S) + 1) for _ in range(8)]
+D = [[0] * (8) for _ in range(N + 1)]
 D[0][0] = 1
-for i in range(1, len(S)):
-    for j in range(1, 8):
+for i in range(len(S)):
+    for j in range(8):
         D[i + 1][j] += D[i][j]
         if S[i] == "a" and j == 0:
             D[i + 1][j + 1] += D[i][j]
@@ -13,15 +13,12 @@ for i in range(1, len(S)):
             D[i + 1][j + 1] += D[i][j]
         if S[i] == "o" and j == 3:
             D[i + 1][j + 1] += D[i][j]
-        if S[i] == "r" and j == 4:
+        if S[i] == "d" and j == 4:
             D[i + 1][j + 1] += D[i][j]
-        if S[i] == "d" and j == 5:
+        if S[i] == "e" and j == 5:
             D[i + 1][j + 1] += D[i][j]
-        if S[i] == "e" and j == 6:
+        if S[i] == "r" and j == 6:
             D[i + 1][j + 1] += D[i][j]
-        if S[i] == "r" and j == 7:
-            D[i + 1][j + 1] += D[i][j]
-        print(i, j, D[i + 1][j])
     for j in range(8):
         D[i + 1][j] %= 1000000007
 print(D[-1][-1])
