9528bb5049889ca746657fcfe67c51c6a0b584f9
keimoriyama <keischwiiz@gmail.com>
2022-09-30 10:58:27 +0900

    solved 214

---
+++ ABC/214/c.py
@@ -0,0 +1,9 @@
+N = int(input())
+S = list(map(int, input().split()))
+T = list(map(int, input().split()))
+
+for i in range(2 * N):
+    T[(i + 1) % N] = min(T[(i + 1) % N], T[i % N] + S[i % N])
+
+for a in T:
+    print(a)
