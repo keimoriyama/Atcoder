f2cf4fc7a23a69f668fae115abb4ba5c9593b6f9
kei <keischwiiz@gmail.com>
2024-09-27 20:33:07 +0900

    update

--- ABC/372/c.py
+++ ABC/372/c.py
@@ -1,1 +1,25 @@
-N, Q= map(int,input().split())
+N, Q = map(int, input().split())
+S = list(input())
+
+abc = ["A", "B", "C"]
+ans = 0
+for i in range(N - 2):
+    if S[i : i + 3] == abc:
+        ans += 1
+        
+prev_S = S
+for i in range(Q):
+    X, C = input().split()
+    X = int(X) - 1
+    for k in range(3):
+        idx = X - k
+        if 0 <= idx and idx + 2 < N:
+            if S[idx : idx + 3] == abc:
+                ans -= 1
+    S[X] = C
+    for k in range(3):
+        idx = X - k
+        if 0 <= idx and idx + 2 < N:
+            if S[idx : idx + 3] == abc:
+                ans += 1
+    print(ans)
